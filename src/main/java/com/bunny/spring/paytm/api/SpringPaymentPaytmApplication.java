e32c240b648bdba515e4bd05e90c4ba5de7656ac
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-07-14 03:34:27 +0530

    spring-boot-paytm-integration

---
+++ src/main/java/com/bunny/spring/paytm/api/SpringPaymentPaytmApplication.java
@@ -0,0 +1,13 @@
+package com.bunny.spring.paytm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaymentPaytmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaymentPaytmApplication.class, args);
+	}
+
+}
